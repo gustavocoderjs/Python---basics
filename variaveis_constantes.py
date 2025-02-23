b21dd439902de3e71d9b445ccff6ee2279307d31
Gustavo <gustavocoderjs@gmail.com>
2025-02-23 11:02:22 -0300

    Aula sobre variáveis e constantes

---
+++ variaveis_constantes.py
@@ -0,0 +1,14 @@
+
+nome, idade = "Giovanna", 27
+
+print (nome, idade)
+
+limite_saque_diario = 10000
+
+print (limite_saque_diario)
+
+BRAZILIAN_STATES = ["SP", "RJ", "MG", "GO", "SC", "BA", "AL"]
+
+print (BRAZILIAN_STATES)
+
+
