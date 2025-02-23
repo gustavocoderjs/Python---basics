d4a99909104c8e24eb794e89cd35721f5dcc15dd
Gustavo <gustavocoderjs@gmail.com>
2025-02-23 11:31:58 -0300

    Conversão de tipos em Python

---
+++ convertendo_tipos.py
@@ -0,0 +1,19 @@
+print(int(1.984452445))
+print(int("10"))
+print(float("10.10"))
+print(float(100))
+print(type(str(10.10)))
+
+valor_um = 10 
+valor_str = str(valor_um)
+
+print(type(valor_um))
+print(type(valor_str))
+
+print (100 / 2)
+print (100 // 2)
+
+
+
+  
+
