6c3b73688cbf36857967042f6369aed5b48b4794
Gustavo <gustavocoderjs@gmail.com>
2025-02-23 12:54:18 -0300

    Aula sobre os comandos print/input e separadores

---
+++ entrada_saida.py
@@ -0,0 +1,10 @@
+nome = "Guilherme"
+sobrenome = "Carvalho"
+
+print(nome, sobrenome)
+print(nome, sobrenome, end="...\n")
+
+nome_input = input("Informe seu nome: ")
+idade_input = input("Informe sua idade: ")
+
+print(nome_input, idade_input, end="...\n")
