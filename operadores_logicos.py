bc9e385952c2415d84e57f072f3fba4b1c0d111b
Gustavo <gustavocoderjs@gmail.com>
2025-02-24 22:16:20 -0300

    Aula sobre operadores lógicos

---
+++ operadores_logicos.py
@@ -0,0 +1,23 @@
+
+saldo = 1000
+saque = 250
+limite = 200 
+conta_especial = True
+
+# Na condição AND: para ser True, tudo precisa ser True
+# Na condição OR: 
+print(True and True)
+print(True and False)
+print(False and False)
+print(True or True)
+print(True or False)
+
+expressao = (saldo >= saque  and saque <= limite) or (conta_especial and saldo >= saque)
+print(expressao)
+
+conta_normal_com_saldo = saldo >= saque and saque <= limite
+conta_especial_com_saldo = conta_especial and saldo >= saque
+
+expressao_dois = conta_especial_com_saldo or conta_especial_com_saldo
+print (expressao_dois)
+
