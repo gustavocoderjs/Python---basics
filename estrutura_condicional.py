dcb9bde4539b2dfb72c33068afa37bdfa5b0fbb5
Gustavo <gustavocoderjs@gmail.com>
2025-02-26 00:35:07 -0300

    Aula sobre estrutura condicional

---
+++ estrutura_condicional.py
@@ -0,0 +1,22 @@
+
+saldo = 2000 
+saque = float(input("Informe o valor do saque: "))
+
+if saldo >= saque:
+    print("Saque realizado! ")
+else:
+    print("Saldo insuficiente! ")
+
+maior_idade = 18 
+
+idade = int(input("Informe sua idade: "))
+
+if idade >= maior_idade: 
+    print("Usuário maior de idade, habilitação ok!")
+else:
+    print("Usuário menor de idade, habilitação recusada")
+
+numero = 5 
+mensagem = "Par" if numero % 2 == 0 else "Ímpar" 
+print(mensagem) #Saída: Ímpar 
+
