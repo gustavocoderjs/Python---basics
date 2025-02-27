65626f38ed8637ec1a69fd4059dd4782beb1bc2a
Gustavo <gustavocoderjs@gmail.com>
2025-02-27 01:05:23 -0300

    Aula sobre estruturas de repetição

---
+++ estruturas_repeticao.py
@@ -0,0 +1,21 @@
+
+i = 1 
+
+while i < 10: 
+    print(i)
+    i +=1
+
+print("Fim da execução")
+print(i)
+
+criancas = ["Manu", "Vini", "Selina"]
+
+for item in criancas:
+    print(item)
+
+canal = "Refatorando"
+
+for letra in canal:
+    print(letra)
+    
+    
---
+++ teste.py
@@ -0,0 +1,14 @@
+
+matriz_numeros = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+]
+
+for linha in matriz_numeros:
+    print("---")
+    for coluna in linha:
+        print(coluna)
+        
+
+    
