e8d3d7c0227b9b981adcc343b894cf38b51dd5dd
Gustavo <gustavocoderjs@gmail.com>
2025-02-27 01:06:25 -0300

    Adicionado arquivo de teste

--- estruturas_repeticao.py
+++ estruturas_repeticao.py
@@ -18,4 +18,35 @@ canal = "Refatorando"
 for letra in canal:
     print(letra)
     
+for index in range(20):
+    print(index)
+    
+for index in range (len(criancas)):
+    print(index)
+    
+
+criancas = ["Duda", "Gabriel", "Marcela"]
+for index in range (len(criancas)):
+    print(criancas[index], index)
+    
+for index in range(3):
+    if index == 0:
+        print("primeira linha")
+    else:
+        print(f"outras linhas {index}")
+        
+   
+
+matriz_numeros = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+]
+
+for linha in matriz_numeros:
+    print("---")
+    for coluna in linha:
+        print(coluna)
+        
+
     
