111e4019734dcc9b0648644c0e3afa9dc97d030f
Gustavo <gustavocoderjs@gmail.com>
2025-03-04 23:58:12 -0300

    Arquivo salvo

--- operadores_associacao.py
+++ operadores_associacao.py
@@ -7,6 +7,10 @@ print("Python" in curso)
 print("abacate" not in frutas)
 print(200 in saques)
 
+frotas = ["maritma", "aerea", 'terrestre']
+print(frotas)
+
+print("aereas" in frotas)
 
 # Operadores utilizados para saber se um objeto está presente ek uma sequência
 
