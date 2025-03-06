92d8ac74b2aec9aff203db851cbae6d423f270db
Gustavo <gustavocoderjs@gmail.com>
2025-03-05 22:02:50 -0300

    Aula sobre como manipular strings no Python

---
+++ classe_string.py
@@ -0,0 +1,31 @@
+curso = "PythoN"
+print(curso.upper())
+print(curso.lower())
+print(curso.title())
+
+eliminar = " Espaço "
+print(eliminar.strip()) # remove todo o espaço 
+print(eliminar.lstrip()) # remove espaço lado esquerdo
+print(eliminar.rstrip()) #remove espaço lado esquerdo
+
+cursar = "Python"
+print(cursar.center(10, "#"))
+print(";".join(cursar))
+
+nome = "Guilherme"
+idade = 28 
+profissao = "programador"
+linguagem = "Go"
+
+print (f"Olá, me chamo {nome} e tenho {idade} anos de idade. Atualmente trabalho como {profissao} e estou matriculado no curso de {linguagem}")
+ 
+
+meuNome = "Gustavo Cordeiro de Souza"
+print(meuNome[0])
+print(meuNome[:9])
+print(meuNome[10:16])
+print(meuNome[:])
+print(meuNome[::-1])
+
+
+ 
