b407d80f10fd395785d539f12a6f1d3258d03060
Gustavo <gustavocoderjs@gmail.com>
2025-05-14 09:55:26 -0300

    Arquivos adicionados: list comprehesion

---
+++ list_comprehesion.py
@@ -0,0 +1,13 @@
+
+
+squares = []
+for i in range (1, 101):
+    squares.append(i**2)
+print(squares)
+
+
+# Other way to write the same question 
+
+squares2 = [i**2 for i in range(1, 101)]
+print (squares2)
+
---
+++ list_comprehesion2.py
@@ -0,0 +1,4 @@
+
+
+remainders5 = [ x**2 % 5 for x in range (1, 101)]
+print(remainders5)
