4313457006246b00a77d38d79675e9513ab5cb70
Gustavo <gustavocoderjs@gmail.com>
2025-05-16 07:26:28 -0300

    Adicionado arquivo: set.py

---
+++ set.py
@@ -0,0 +1,30 @@
+
+
+conjunto = { "python", "java", "python" } 
+
+print(conjunto)
+
+# Conjuntos em python não suportam indexação e nem fatiamento, caso queira acessar
+# os seus valores é necessário converter o conjunto para uma lista 
+
+conjuntoDois = list(conjunto)
+print(conjuntoDois[0])
+
+# A forma mais comum para percorrer os dados de um conjunto é utilizando o comando for 
+
+carros = {'gol', 'celta', 'hb20', 'fiesta'}
+
+for carro in carros: 
+    print(carro)
+    
+    
+for indice, carro in enumerate(carros): 
+    print(f'{indice}: {carro}')
+    
+conjuntoA = {1, 2}
+conjuntoB = {9, 0}
+
+print(conjuntoA.union(conjuntoB))
+
+
+
